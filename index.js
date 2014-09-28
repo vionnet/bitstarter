533cc812f9488c993452209f65de95c358764092
Ubuntu <ubuntu@ip-172-31-1-252.us-west-1.compute.internal>
2014-09-28 20:14:15 +0000

    Add index.html and update web.js

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-var express = require('express')
-var app = express();
+var express = require('express');
+var app = express.createServer(express.logger());
+var fs = require('fs');
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+var text = fs.readFileSync('/home/ubuntu/bitstarter/index.html');
+var buffer = new Buffer(text, "utf-8");
 
-app.get('/', function(request, response) {
-  response.send('Hello World 2!')
-})
+app.get('/', function(request, response){
+   response.send(buffer.toString());
+});
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+   console.log("Listening on " + port);
+
+});
+
