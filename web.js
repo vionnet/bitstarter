5cb27df992027da227983c90e288742fe004c65e
vionnet <anyu.slofstra@gmail.com>
2014-09-28 20:41:35 +0000

    delete index.js

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
+
+var text = fs.readFileSync("/home/ubuntu/bitstarter/index.html");
+var buffer = new Buffer(text, "utf-8");
 
 var app = express.createServer(express.logger());
-
 app.get('/', function(request, response){
-   response.send('My learning app');
+   response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
@@ -11,3 +14,4 @@ app.listen(port, function() {
    console.log("Listening on " + port);
 
 });
+
