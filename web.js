575d824cdb72c1b16698e4998c084d44af58cb6a
vionnet <anyu.slofstra@gmail.com>
2014-09-28 21:25:24 +0000

    update web.js

--- web.js
+++ web.js
@@ -1,4 +1,22 @@
 var express = require('express');
+var app = express.createServer(express.logger());
+
+// way to do it without a buffer since fs.readFileSync
+// says if you include encoding option ('utf-8') in 
+// this case then this function returns a string
+app.get('/', function(request, response) {
+  var fs = require('fs');
+  var content = fs.readFileSync('index.html', 'utf-8');
+  response.send(content);
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
+
+/*
+var express = require('express');
 var fs = require('fs');
 
 var text = fs.readFileSync("/home/ubuntu/bitstarter/index.html");
@@ -13,5 +31,5 @@ var port = process.env.PORT || 5000;
 app.listen(port, function() {
    console.log("Listening on " + port);
 
-});
+});*/
 
